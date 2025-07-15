aee6f345a245a7406d511e12ecfcc81bbaf9a4ba
Littlesock <144090458+Wuhaoze233@users.noreply.github.com>
2025-07-15 20:47:30 +0800

    lesson 13

--- 14/combinatorial_number.py
+++ 14/combinatorial_number.py
@@ -2,18 +2,12 @@
 求组合数 Combinatorial number C(m, n)
 
 Author: Haoze Wu
-Version: 1.0
+Version: 1.1
 '''
 
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    elif n < 0:
-        raise ValueError("Factorial is not defined for negative numbers.")
-    else:
-        return n * factorial(n - 1)
+from math import factorial as f
 
 m = int(input('m = '))
 n = int(input('n = '))
-combinatorial_num = factorial(n) / (factorial(m) * factorial(n - m))
+combinatorial_num = f(n) / (f(m) * f(n - m))
 print(f'The combinatorial number C({m}, {n}) is {combinatorial_num:.0f}')
